91b484987eb74a388db397473603322e0161c347
Raj Ghugare <rajghugare.vnit@gmail.com>
2021-05-24 14:03:29 +0530

    working buffer

--- dreamerv2/buffers/buffers.py
+++ dreamerv2/buffers/buffers.py
@@ -1,10 +1,11 @@
 import numpy as np 
 import torch 
 import random 
-from dreamerv2.utils import Episode
 from typing import Tuple
 from collections import namedtuple, deque
 
+
+Episode = namedtuple('Episode', ['obs', 'act', 'rew', 'nonterms', 'length'])  
 
 class EpisodicBuffer():
     """
@@ -17,7 +18,7 @@ class EpisodicBuffer():
         self,
         max_episodes,
         obs_shape: Tuple[int],
-        action_shape: Tuple[int],
+        action_size: int,
         obs_type=np.float32,
         action_type=np.float32,
         pixels=False,  
@@ -30,9 +31,8 @@ class EpisodicBuffer():
         :params full: indicates whether the entire buffer is filled with transitions
         :params total_episodes: total episodes stored in the buffer
         """
-        
         self.max_episodes = max_episodes
-        self.action_shape = action_shape
+        self.action_size = action_size
         self.action_dtype = action_type 
         self.obs_shape = obs_shape
         self.obs_dtype = obs_type
@@ -44,23 +44,21 @@ class EpisodicBuffer():
         self._init_episode()
         self.memory = deque([],maxlen=max_episodes)
     
-    def add(self,obs,act=None,rew=None,done=None):
+    def add(self, obs, act=None, rew=None, done=None):
         self.obs.append(obs)
-        if act==None:
-            pass
-        else:  
-            self.act.append(act)
-            self.rew.append(rew)
-            self.nonterms.append(not done)
-        
+        if done==None:
+            act = np.zeros(self.action_size, dtype=self.action_dtype)
+            rew = 0.0
+            done = False 
+        self.act.append(act)
+        self.rew.append(rew)
+        self.nonterms.append(not done)
         if done:
             self.add_episode()
-            self._init_episode()
             
     def add_episode(self):
         assert self.nonterms[-1] == False
         e = Episode(*self._episode_toarray(), len(self.obs))
-        
         self.memory.append(e)
         self.total_episodes += 1
         if self.total_episodes == self.max_episodes:
@@ -68,15 +66,17 @@ class EpisodicBuffer():
         self._init_episode()
     
     def sample(self, seq_len, batch_size):
-        episode_list = random.choices(self.memory,k=batch_size)
+        episode_list = random.choices(self.memory, k=batch_size)
         obs_batch = np.zeros([seq_len, batch_size, *self.obs_shape], dtype=self.obs_dtype)
-        act_batch = np.zeros([seq_len, batch_size, *self.action_shape], dtype=self.action_dtype)
+        act_batch = np.zeros([seq_len, batch_size, self.action_size], dtype=self.action_dtype)
         rew_batch = np.zeros([seq_len, batch_size], dtype=np.float32)
         nonterm_batch = np.zeros([seq_len, batch_size], dtype=bool)
-        
         for ind,episode in enumerate(episode_list):
             obs_batch[:,ind], act_batch[:,ind], rew_batch[:,ind], nonterm_batch[:,ind] = self._sample_seq(episode, seq_len)
         return obs_batch, act_batch, rew_batch , nonterm_batch
+    
+    def sample(self, seq_len, batch_size):
+        episode_list = random.choices(self.memory, k=batch_size)
     
     def _episode_toarray(self):
         o = np.stack(self.obs, axis=0)
@@ -86,8 +86,14 @@ class EpisodicBuffer():
         return o,a,r,nt
     
     def _sample_seq(self, episode, seq_len):
+        assert episode.length>=seq_len
         s = min(np.random.choice(episode.length),episode.length-seq_len)
-        return np.array(episode.obs)[s:s+seq_len], np.array(episode.actions)[s:s+seq_len], np.array(episode.rewards)[s:s+seq_len], np.array(episode.nonterms)[s:s+seq_len]
+        return (
+                np.array(episode.obs)[s:s+seq_len], 
+                np.array(episode.act)[s:s+seq_len], 
+                np.array(episode.rew)[s:s+seq_len], 
+                np.array(episode.nonterms)[s:s+seq_len]
+            )
         
     def _init_episode(self):
         self.obs = [] 
@@ -102,24 +108,3 @@ class EpisodicBuffer():
     @property
     def current_episodes(self):
         return self.total_episodes
-    
-class FixedEpisodicBuffer(object):
-    """
-    Can be used if all episodes are of fixed length and the length is known a prior
-    """
-    def __init__(
-        self, 
-        action_size, 
-        observation_size,
-        max_episode_length, 
-        pixels=False, 
-        buffer_size=10 ** 6, 
-        bits=5, 
-        device="cpu"
-    ):
-        """
-        
-        :param buffer_size: maximum number of transitions that can be stored
-        :param pixels: true if observations are images
-        """
-        raise NotImplementedError
