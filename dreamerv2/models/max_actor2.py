fixed gradients

dreamerv2/models/max_actor2.py
@@ -155,7 +155,7 @@ class MaxActionModel(nn.Module):
             raise Exception('invalid utility measure')
         
     def forward(self):
-        action, action_dist = self.do_max_exploration(None, None)
+        action, action_dist = self.do_max_exploration(None)
         return action, action_dist
 
     def get_action_dist(self, modelstate):
@@ -185,9 +185,9 @@ class MaxActionModel(nn.Module):
             self.buffer.train_batches(batch_size=self.batch_size):
 
             self.optimizer.zero_grad()
-            with torch.enable_grad():
-                loss = self.model.loss(tr_states, tr_actions, tr_state_deltas, 
-                    training_noise_stdev=self.training_noise_stdev)
+            # with torch.enable_grad():
+            loss = self.model.loss(tr_states, tr_actions, tr_state_deltas, 
+                training_noise_stdev=self.training_noise_stdev)
             losses.append(loss.item())
             loss.backward()
             torch.nn.utils.clip_grad_value_(self.model.parameters(), self.grad_clip)
@@ -259,7 +259,10 @@ class MaxActionModel(nn.Module):
  
             r = self.utility(s, a, ns, mu, var, self.model)
             self.agent.replay.add_batch(s, ns, a, r, np.zeros(0), [{}])
-        
+            # s : [size, state_size], r : [size]
+            wandb.log({'max/utility_mean' : r.mean(),
+                       'max/utility_min' : r.min(),
+                       'max/utility_max' : r.max()})
 
     def get_action(self):
         current_state = self.mdp.reset()

dreamerv2/models/sac_discrete.py
@@ -422,15 +422,15 @@ class SAC(nn.Module):
             next_q_value = data.rewards.flatten() + (1 - data.dones.flatten()) * self.args.gamma * (min_qf_next_target)
                             #[64, 1]                   [64, 1]                                         [64, 3 ]
 
-        with torch.enable_grad():
-            # use Q-values only for the taken actions
-            qf1_values = self.qf1(data.observations)
-            qf2_values = self.qf2(data.observations)
-            qf1_a_values = qf1_values.squeeze().gather(1, data.actions.argmax(1).unsqueeze(1).long()).view(-1)
-            qf2_a_values = qf2_values.squeeze().gather(1, data.actions.argmax(1).unsqueeze(1).long()).view(-1)
-            qf1_loss = F.mse_loss(qf1_a_values, next_q_value)
-            qf2_loss = F.mse_loss(qf2_a_values, next_q_value)
-            qf_loss = qf1_loss + qf2_loss
+        # with torch.enable_grad():
+        # use Q-values only for the taken actions
+        qf1_values = self.qf1(data.observations)
+        qf2_values = self.qf2(data.observations)
+        qf1_a_values = qf1_values.squeeze().gather(1, data.actions.argmax(1).unsqueeze(1).long()).view(-1)
+        qf2_a_values = qf2_values.squeeze().gather(1, data.actions.argmax(1).unsqueeze(1).long()).view(-1)
+        qf1_loss = F.mse_loss(qf1_a_values, next_q_value)
+        qf2_loss = F.mse_loss(qf2_a_values, next_q_value)
+        qf_loss = qf1_loss + qf2_loss
         
         self.q_optimizer.zero_grad()
         qf_loss.backward()
@@ -442,26 +442,26 @@ class SAC(nn.Module):
         self.q_optimizer.step()
 
         # ACTOR training
-        with torch.enable_grad():
-            _, log_pi, action_probs = self.actor.get_action(data.observations)
+        # with torch.enable_grad():
+        _, log_pi, action_probs = self.actor.get_action(data.observations)
     
         with torch.no_grad():
             qf1_values = self.qf1(data.observations)
             qf2_values = self.qf2(data.observations)
             min_qf_values = torch.min(qf1_values, qf2_values)
         
-        with torch.enable_grad():
-            # no need for reparameterization, the expectation can be calculated for discrete actions
-            actor_loss = (action_probs * ((self.alpha * log_pi) - min_qf_values)).mean()
+        # with torch.enable_grad():
+        # no need for reparameterization, the expectation can be calculated for discrete actions
+        actor_loss = (action_probs * ((self.alpha * log_pi) - min_qf_values)).mean()
 
         self.actor_optimizer.zero_grad()
         actor_loss.backward()
         self.actor_optimizer.step()
 
         if self.args.autotune:
-            with torch.enable_grad():
-                # re-use action probabilities for temperature loss
-                alpha_loss = (action_probs.detach() * (-self.log_alpha * (log_pi + self.target_entropy).detach())).mean()
+            # with torch.enable_grad():
+            # re-use action probabilities for temperature loss
+            alpha_loss = (action_probs.detach() * (-self.log_alpha * (log_pi + self.target_entropy).detach())).mean()
 
             self.a_optimizer.zero_grad()
             alpha_loss.backward()
@@ -496,4 +496,3 @@ class SAC(nn.Module):
         self.glob_agent_step += 1
         
         return self.global_update_step
-        

test/pomdp.py
@@ -34,7 +34,7 @@ def init_wandb(dreamer_args, minatar_args_dict, sac_args, max_args=None):
 
     wandb.init(
         project='dreamermax',
-        name='kate2_experiments_with_configs',
+        name='FIXED_MODEL',
         sync_tensorboard=False,
         config=config,
         monitor_gym=True,
@@ -126,19 +126,20 @@ def main(args):
             _, posterior_rssm_state = trainer.RSSM.rssm_observe(embed, prev_action, not done, prev_rssmstate)
             model_state = trainer.RSSM.get_model_state(posterior_rssm_state)
             
-            ### !11111111111111111111111111111111111111111111111111111
-            if False: #iter < 10**5:
-                action, action_dist = trainer.ActionModel(model_state)
-                action = trainer.ActionModel.add_exploration(action, iter).detach()
-                action_ent = torch.mean(action_dist.entropy()).item()
-                episode_actor_ent.append(action_ent)
-            else:
-                action, action_probs = maxtrainer()
-                # print(f"{action_probs}")
-                action_dist = torch.distributions.OneHotCategorical(probs=action_probs)
-                action_ent = torch.mean(action_dist.entropy()).item()
-                episode_actor_ent.append(action_ent)
-            ### !11111111111111111111111111111111111111111111111111111
+            # ### !11111111111111111111111111111111111111111111111111111
+            # if False: #iter < 10**5:
+            #     action, action_dist = trainer.ActionModel(model_state)
+            #     action = trainer.ActionModel.add_exploration(action, iter).detach()
+            #     action_ent = torch.mean(action_dist.entropy()).item()
+            #     episode_actor_ent.append(action_ent)
+            # else:
+
+        action, action_probs = maxtrainer()
+        # print(f"{action_probs}")
+        action_dist = torch.distributions.OneHotCategorical(probs=action_probs)
+        action_ent = torch.mean(action_dist.entropy()).item()
+        episode_actor_ent.append(action_ent)
+        ### !11111111111111111111111111111111111111111111111111111
             
         # next_obs, rew, done, _ = env.step(action.squeeze(0).cpu().numpy())
         next_obs, rew, done, _ = env.step(action) #.cpu().numpy())